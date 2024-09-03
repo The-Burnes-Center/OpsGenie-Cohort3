44ec68d83dbb34a2f438979268e3288262d71ac8
Jake-Ashkenase <116736922+Jake-Ashkenase@users.noreply.github.com>
2024-09-03 16:32:34 -0400

    changing constants

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -1,5 +1,5 @@
 export declare const AUTHENTICATION = true;
-export declare const kendraIndexName = "prod-massHealth";
-export declare const cognitoDomainName = "genaimvp-auth-massHealth";
+export declare const kendraIndexName = "prod-mec";
+export declare const cognitoDomainName = "mec-chatbot-auth";
 export declare const OIDCIntegrationName = "";
-export declare const stackName = "GenAiChatStack-massHealth";
+export declare const stackName = "MECKnowledgeStack";
