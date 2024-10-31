3463925047cfcfa38b3e1956d5657ecfc467af13
Temi A <akinyoade.t@northeastern.edu>
2024-10-31 14:15:26 -0400

    without ui stuff

--- lib/user-interface/app/src/themed/components/app-layout/visual-refresh-toolbar/contexts.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface BreadcrumbsSlotContextType {
-    isInToolbar: boolean;
-}
-export declare const BreadcrumbsSlotContext: React.Context<BreadcrumbsSlotContextType | undefined>;
-export {};
-//# sourceMappingURL=contexts.d.ts.map
--- lib/user-interface/app/src/themed/components/internal/components/file-dropzone/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { FileDropzoneProps } from './interfaces';
-import { useFilesDragging } from './use-files-dragging';
-export { FileDropzoneProps, useFilesDragging };
-export default function InternalFileDropzone({ onChange, children }: FileDropzoneProps): JSX.Element;
-//# sourceMappingURL=index.d.ts.map
