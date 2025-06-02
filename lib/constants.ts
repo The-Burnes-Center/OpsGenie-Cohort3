cc579e9f69a469fae66ea20852c0a4ad524caf7c
Pra-soon <175355503+Pra-soon@users.noreply.github.com>
2025-06-02 12:31:13 -0400

    Adding integration name OIDC

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,6 +7,6 @@ export const kendraIndexName = 'itops-kendra-index'
 export const cognitoDomainName = "itops-hhs-chatbot"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
-export const OIDCIntegrationName = ""
+export const OIDCIntegrationName = "OpsGenie"
 // this MUST be unique to your account
 export const stackName = "ITOPSRAGStack"
