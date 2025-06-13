f19a739b2d6645dfebca4f7e421b2a2b5fd42e59
Pra-soon <175355503+Pra-soon@users.noreply.github.com>
2025-06-13 12:09:20 -0400

    trying new deployment

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,11 +2,11 @@ export const AUTHENTICATION = true;
 
 // change these as needed
 // kendraIndexName - must be unique to your account
-export const kendraIndexName = 'itops-kendra-index'
+export const kendraIndexName = 'itops-test-kendra-index'
 // must be unique globally or the deployment will fail
-export const cognitoDomainName = "itops-hhs-chatbot"
+export const cognitoDomainName = "itops-test-hhs-chatbot"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
 export const OIDCIntegrationName = "OpsGenie"
 // this MUST be unique to your account
-export const stackName = "ITOPSRAGStack"
+export const stackName = "ITOPSTestRAGStack"
