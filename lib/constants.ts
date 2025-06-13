e51d24d28905f99fbdd5b7a45583073a8fbf450c
Pra-soon <175355503+Pra-soon@users.noreply.github.com>
2025-06-13 10:20:29 -0400

    Revert "deploying test stack"
    This reverts commit 970987da601f66f499fb35876a1bf2173a9375d1.

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,11 +2,11 @@ export const AUTHENTICATION = true;
 
 // change these as needed
 // kendraIndexName - must be unique to your account
-export const kendraIndexName = 'opsgenie-kendra-index'
+export const kendraIndexName = 'itops-kendra-index'
 // must be unique globally or the deployment will fail
-export const cognitoDomainName = "opsgenie-hhs-chatbot"
+export const cognitoDomainName = "itops-hhs-chatbot"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
-export const OIDCIntegrationName = ""
+export const OIDCIntegrationName = "OpsGenie"
 // this MUST be unique to your account
-export const stackName = "TestOpsGenieStack"
+export const stackName = "ITOPSRAGStack"
