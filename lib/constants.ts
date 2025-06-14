c5523adb11526c2183bbda5da268a4a8bb6a4417
Pra-soon <175355503+Pra-soon@users.noreply.github.com>
2025-06-14 19:46:39 -0400

    Revert "Retest Deploy"
    This reverts commit 9761a4da05539b0b2b9c24d6818893c504313f2b.

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,14 +2,11 @@ export const AUTHENTICATION = true;
 
 // change these as needed
 // kendraIndexName - must be unique to your account
-// export const kendraIndexName = 'itops-kendra-index'
-export const kendraIndexName = 'itops-opsgenie-kendra-index'
+export const kendraIndexName = 'itops-kendra-index'
 // must be unique globally or the deployment will fail
-// export const cognitoDomainName = "itops-hhs-chatbot"
-export const cognitoDomainName = "itops-opsgenie-hhs-chatbot"
+export const cognitoDomainName = "itops-hhs-chatbot"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
 export const OIDCIntegrationName = "OpsGenie"
 // this MUST be unique to your account
-// export const stackName = "ITOPSRAGStack"
-export const stackName = "ITOPSOpsGenieRAGStack"
+export const stackName = "ITOPSRAGStack"
